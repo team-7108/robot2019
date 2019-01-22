fa43df20b3c2a53c9f228dcc6ab6fd48a2e83e32
Emre <emrecandabak@gmail.com>
2019-01-22 17:04:35 +0300

    Fix autonomous bug.

--- src/main/java/frc/robot/commands/Autonomous.java
+++ src/main/java/frc/robot/commands/Autonomous.java
@@ -1,37 +1,12 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.command.CommandGroup;
 
-public class Autonomous extends Command {
+public class Autonomous extends CommandGroup {
   public Autonomous() {
     // Use requires() here to declare subsystem dependencies
     // eg. requires(chassis);
-  }
+  }}
 
-  // Called just before this Command runs the first time
-  @Override
-  protected void initialize() {
-  }
-
-  // Called repeatedly when this Command is scheduled to run
-  @Override
-  protected void execute() {
-  }
-
-  // Make this return true when this Command no longer needs to run execute()
-  @Override
-  protected boolean isFinished() {
-    return false;
-  }
-
-  // Called once after isFinished returns true
-  @Override
-  protected void end() {
-  }
-
-  // Called when another command which requires one or more of the same
-  // subsystems is scheduled to run
-  @Override
-  protected void interrupted() {
-  }
-}
+  
